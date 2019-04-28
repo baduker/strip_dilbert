13fedf1ff76316e0e8be79fc342d2e23eee766e9
baduker <baduker@protonmail.ch>
2019-04-28 10:04:55 -0700

    Added method for LAST_COMIC

--- strip_dilbert.py
+++ strip_dilbert.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3.6
 
 import os
+import time
 import requests
 from bs4 import BeautifulSoup as bs
 from datetime import date, timedelta
@@ -10,10 +11,25 @@ COMICS_DIRECTORY = os.path.join(os.getcwd(), DEFAULT_DIR_NAME)
 
 BASE_URL = "https://dilbert.com/strip/"
 
-d1 = date(1989, 4, 16)  # start date
-d2 = date(1989, 4, 30)  # end date
+FIRST_COMIC = date(1989, 4, 16)  # start date
+# d2 = date(1989, 4, 21)  # end date
 
-def grab_comic_strip_url(start_date, end_date):
+def get_comic_strip_end_date():
+	print("Type a dilbert comic strip date in YYYY/MM/DD format:")
+	while True:
+		start_year, start_month, start_day = map(int, input(">> ").split("/"))
+		end_date = date(start_year, start_month, start_day)
+		if end_date < FIRST_COMIC:
+			print("The oldest comic is from 1989/04/16. Try again.")
+			continue
+		elif end_date > date.today():
+			print("You can't download anything from the future yet. Try again.")
+			continue
+		else:
+			break
+	return end_date
+
+def get_comic_strip_url(start_date, end_date):
   full_url = []
   delta = end_date - start_date
   for day in range(delta.days + 1):
@@ -21,12 +37,12 @@ def grab_comic_strip_url(start_date, end_date):
   return full_url
 
 
-def get_download_src_url(session, response):
+def get_image_comic_url(session, response):
   soup = bs(response.text, 'html.parser')
   for div in soup.find_all('div', class_="img-comic-container"):
     for a in div.find_all('a', class_="img-comic-link"):
       for img in a.find_all('img', src=True):
-        return img['src']
+        return "http:" + img['src']
 
 def download_dilbert(s, u):
   filne_name = u.split('/')[-1]
@@ -34,14 +50,20 @@ def download_dilbert(s, u):
     response = s.get(u)
     file.write(response.content)
 
-url_list = grab_comic_strip_url(d1, d2)
+LAST_COMIC = get_comic_strip_end_date()
+
+start = time.time()
+
+url_list = get_comic_strip_url(FIRST_COMIC, LAST_COMIC)
 
 os.mkdir(DEFAULT_DIR_NAME)
 
 for url in url_list:
   session = requests.Session()
   response = session.get(url)
-  download_url = "http:" + get_download_src_url(session, response)
+  download_url = get_image_comic_url(session, response)
+  print("Downloading {}".format(download_url))
   download_dilbert(session, download_url)
+end = time.time()
 
-print("Files downloaded!")
+print("Files downloaded in {:.2f} seconds!".format(end - start))

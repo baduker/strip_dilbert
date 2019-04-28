cfdfb20c61d044a26e6c80571b0c3402ff397fe8
baduker <baduker@protonmail.ch>
2019-04-28 11:40:22 -0700

    Added start strip date method and tweaked the progress bar

--- strip_dilbert.py
+++ strip_dilbert.py
@@ -13,13 +13,27 @@ COMICS_DIRECTORY = os.path.join(os.getcwd(), DEFAULT_DIR_NAME)
 BASE_URL = "https://dilbert.com/strip/"
 
 FIRST_COMIC = date(1989, 4, 16)  # start date
-# d2 = date(1989, 4, 21)  # end date
 
-def get_comic_strip_end_date():
-	print("Type a dilbert comic strip date in YYYY/MM/DD format:")
+def get_comic_strip_start_date():
+	print("Type a dilbert comic start date in YYYY/MM/DD format:")
 	while True:
 		start_year, start_month, start_day = map(int, input(">> ").split("/"))
-		end_date = date(start_year, start_month, start_day)
+		start_date = date(start_year, start_month, start_day)
+		if start_date < FIRST_COMIC:
+			print("The oldest comic is from 1989/04/16. Try again.")
+			continue
+		elif start_date > date.today():
+			print("You can't download anything from the future yet. Try again.")
+			continue
+		else:
+			break
+	return start_date
+
+def get_comic_strip_end_date():
+	print("Type a dilbert comic end date in YYYY/MM/DD format:")
+	while True:
+		end_year, end_month, end_day = map(int, input(">> ").split("/"))
+		end_date = date(end_year, end_month, end_day)
 		if end_date < FIRST_COMIC:
 			print("The oldest comic is from 1989/04/16. Try again.")
 			continue
@@ -51,11 +65,12 @@ def download_dilbert(s, u):
     response = s.get(u)
     file.write(response.content)
 
-LAST_COMIC = get_comic_strip_end_date()
+first_strip_date = get_comic_strip_start_date()
+last_strip_date = get_comic_strip_end_date()
 
 start = time.time()
 
-url_list = get_comic_strip_url(FIRST_COMIC, LAST_COMIC)
+url_list = get_comic_strip_url(first_strip_date, last_strip_date)
 
 os.mkdir(DEFAULT_DIR_NAME)
 
@@ -65,10 +80,12 @@ for url in url_list:
 	download_url = get_image_comic_url(session, response)
 
 	pbar = tqdm(range(len(url_list)))
+
 	for i in pbar:
+		pbar.set_description("Fetching {}".format(url[8:]))
 		download_dilbert(session, download_url)
-		pbar.set_description("Fetching {}".format(url))
 		
+
 end = time.time()
 
 print("Files downloaded in {:.2f} seconds!".format(end - start))
